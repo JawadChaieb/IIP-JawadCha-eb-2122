dffa2fe391b6bed1d67db423de49e9b930bbd1b0
JawadChaieb <43877437+JawadChaieb@users.noreply.github.com>
2021-11-29 17:42:33 +0100

    2.0

---
+++ 02.IIP-variabelen en berekeningen/Oefening2/ConsoleBMI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleBMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BMI CALCULATOR");
+            Console.WriteLine("==============");
+
+            double lengte;
+            Console.WriteLine("Lengte (in cm):");
+            lengte = Convert.ToInt32(Console.ReadLine());
+            lengte = lengte / 100;
+
+            int gewicht;
+                
+            Console.WriteLine("Gewicht in kg: ");
+            gewicht = Convert.ToInt32(Console.ReadLine());
+
+            double bmi = gewicht / (lengte * lengte);
+            bmi = Math.Round(bmi,2);
+            Console.WriteLine("Je BMI bedraagt: " + bmi);
+        }
+    }
+}
