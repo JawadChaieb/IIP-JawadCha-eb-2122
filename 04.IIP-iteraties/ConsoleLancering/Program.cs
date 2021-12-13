9a34b8b6791e0dfa5d68db68cbe252a1deff7c98
JawadChaieb <43877437+JawadChaieb@users.noreply.github.com>
2021-12-13 16:25:08 +0100

    commit

---
+++ 04.IIP-iteraties/ConsoleLancering/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLancering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoeveel seconden tot lancering?");
+            int seconden = Convert.ToInt32(Console.ReadLine());
+            for (int i = seconden; i != 0; i--)
+                {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Lift off!");
+            Console.ReadLine();
+        }
+    }
+}
