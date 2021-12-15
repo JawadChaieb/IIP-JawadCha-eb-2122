94435153bb4f5aad7a21a8b76bae87ab72a883b5
JawadChaieb <43877437+JawadChaieb@users.noreply.github.com>
2021-12-15 14:00:29 +0100

    commit

---
+++ 04.IIP-iteraties/ConsoleSom/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleSom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int som = 0;
+            int getal;
+            string stop;
+
+
+
+            do
+            {
+                Console.Write("voer en getal in (q om te stoppen): ");
+                stop = Console.ReadLine();
+                if (stop != "q")
+                {
+                    som += Convert.ToInt32(stop);
+                }
+                else
+                    Console.WriteLine($"De som is: {som}");
+
+                } while ( stop != "q");
+            Console.ReadLine();
+
+
+            //Console.Write($"de som is: {som}");
+            //console.readkey();
+        }
+    }
+}
